f0cb721372023fd5318a7f9363489633aae81065
phoenyx2k <phoenyx2k@gmail.com>
2019-11-21 10:26:05 -0800

    do more work

--- main.tf
+++ main.tf
@@ -20,3 +20,38 @@ module "networking" {
   vnet_address_spacing    = var.vnet_address_spacing
   subnet_address_prefixes = var.subnet_address_prefixes
 }
+module "webserver" {
+  source  = "tap-tfe.digitalinnovation.dev/markfaypremera-org/webserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[0]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
+
+module "appserver" {
+  source  = "tap-tfe.digitalinnovation.dev/markfaypremera-org/appserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[1]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
+
+module "dataserver" {
+  source  = "tap-tfe.digitalinnovation.dev/markfaypremera-org/dataserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[2]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
